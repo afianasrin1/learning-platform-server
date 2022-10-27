76588377c9c4e0b9990ec6f6d4bbd6d48d6214c5
unknown <afia.nasrin3e@gmail.com>
2022-10-27 06:35:46 +0600

    image change

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const cors = require("cors");
 const port = process.env.Port || 5000;
 app.use(cors());
 const categories = require("./data/categories.json");
-
+const courses = require("./data/Courses.json");
 app.get("/", (req, res) => {
   res.send("Courses Api Running");
 });
 
 app.get("/courses-categories", (req, res) => {
   res.send(categories);
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = courses.find((course) => course.id === id);
+  res.send(selectedCourses);
 });
 
 app.listen(port, () => {
